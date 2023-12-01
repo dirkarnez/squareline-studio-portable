441470ebbd04172da20461770fb4d4756a4b5e12
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-01 16:15:53 +0800

    - update run-without-admin.cmd

--- run-without-admin.cmd
+++ run-without-admin.cmd
@@ -1,4 +1,4 @@
 @echo off
 REM https://stackoverflow.com/questions/37878185/what-does-compat-layer-actually-do
-cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" .\SquareLine_Studio_1.2.3_Setup.exe /s /d "%cd%\installation""
+cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && .\SquareLine_Studio_1.2.3_Setup.exe /s /d "%cd%\installation""
 pause

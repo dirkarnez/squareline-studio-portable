1f14b183784c6ccc43f247a25549da1de950df1e
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-01 16:02:32 +0800

    - upload files

---
+++ run-without-admin.cmd
@@ -0,0 +1,4 @@
+@echo off
+REM https://stackoverflow.com/questions/37878185/what-does-compat-layer-actually-do
+cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" .\SquareLine_Studio_1.2.3_Setup.exe /s"
+pause
